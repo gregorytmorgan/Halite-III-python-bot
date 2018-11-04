137da994a6f0a9395c0a0a754a1f85ea7685ceb6
gmorgan <gmorgan@soleranetworks.com>
2018-11-04 10:23:46 -0700

    1) Read version file and update bot name. 2) Track ship status per tutorial

--- MyBot.py
+++ MyBot.py
@@ -17,14 +17,33 @@ import random
 #   (print statements) are reserved for the engine-bot communication.
 import logging
 
+import os
+
 """ <<<Game Begin>>> """
 
 # This game object contains the initial game state.
 game = hlt.Game()
+
+ship_status = {}
+
 # At this point "game" variable is populated with initial map data.
 # This is a good place to do computationally expensive start-up pre-processing.
 # As soon as you call "ready" function below, the 2 second per turn timer will start.
-game.ready("MyPythonBot")
+
+if os.path.exists("./version.txt"):
+    try:
+        version_file = open("./version.txt", 'r')
+        version = version_file.read().strip()
+        version_file.close()
+    except IOError:
+        logging.info("Version file read failed")
+        version = "X"
+else:
+    version = "X"
+
+BotName = "MyBot" + version
+
+game.ready(BotName)
 
 # Now that your bot is initialized, save a message to yourself in the log file with some important information.
 #   Here, you log here your id, which you can always fetch from the game object by using my_id.
@@ -46,18 +65,54 @@ while True:
 
     for ship in me.get_ships():
         # For each of your ships, move randomly if the ship is on a low halite location or the ship is full.
-        #   Else, collect halite.
+        # Else, collect halite.
+
+        if ship.id not in ship_status:
+            ship_status[ship.id] = "exploring"
+
+        logging.info("Ship {} has {} halite and is {}".format(ship.id, ship.halite_amount, ship_status[ship.id]))
+
+        if ship_status[ship.id] == "returning":
+            if ship.position == me.shipyard.position:
+                logging.info("Ship - Drop-off")
+                ship_status[ship.id] = "exploring"
+            else:
+                # move = closes dropoff or shipyard
+                dropoffs = me.get_dropoffs()
+                destinations = list(dropoffs) + [me.shipyard.position]
+
+                minDistance = False
+                movePosition = False
+                move = False
+
+                logging.info("Game - destinations: {}".format(destinations))
+
+                for dest in destinations:
+                    distance = game_map.calculate_distance(ship.position, dest)
+                    if minDistance == False or distance < minDistance:
+                        minDistance = distance
+                        movePosition = dest
+
+                logging.info("Game - movePostition: {}".format(movePosition))
+
+                move = game_map.naive_navigate(ship, movePosition)
+                command_queue.append(ship.move(move))
+                continue
+
+        elif ship.halite_amount >= constants.MAX_HALITE / 4:
+            ship_status[ship.id] = "returning"
+
         if game_map[ship.position].halite_amount < constants.MAX_HALITE / 10 or ship.is_full:
             command_queue.append(
-                ship.move(
-                    random.choice([ Direction.North, Direction.South, Direction.East, Direction.West ])))
+                ship.move(random.choice(["n", "s", "e", "w"])))
         else:
             command_queue.append(ship.stay_still())
 
     # If the game is in the first 200 turns and you have enough halite, spawn a ship.
     # Don't spawn a ship if you currently have a ship at port, though - the ships will collide.
-    if game.turn_number <= 200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
+    if ((game.turn_number % 20) == 0 or game.turn_number == 1) and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
         command_queue.append(me.shipyard.spawn())
+        logging.info("Ship - Spawn")
 
     # Send your moves back to the game environment, ending this turn.
     game.end_turn(command_queue)
