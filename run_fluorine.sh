9d2133e1d1a9bc74e52bf2069b58db53d06f6d9f
gmorgan <gmorgan@soleranetworks.com>
2018-11-04 10:20:31 -0700

    .gitignore
    Update gitignore

--- run_fluorine.sh
+++ run_fluorine.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-TargetFile=$(ls -t -1  ~/dev/Halite-III-python-bot/replays/*.hlt | tail -1)
+TargetFile=$(ls -r -t -1  ~/dev/Halite-III-python-bot/replays/*.hlt | tail -1)
 
-/usr/local/bin/electron /home/gmorgan/dev/fluorine -o  $TargetFile
+
+if [ -f /usr/local/bin/electron ]; then
+    ELECTRON=/usr/local/bin/electron
+elif [ -f node_modules/.bin/electron ]; then 
+    ELECTRON=node_modules/.bin/electron
+else
+    echo 'Electron not found'
+    exit 1
+fi
+
+$ELECTRON /home/gmorgan/dev/fluorine -o  $TargetFile
 
 
 
