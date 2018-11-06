6c09a117430edd3f5e6c23cbc94cf5176faf6992
gmorgan <gmorgan@soleranetworks.com>
2018-11-05 17:04:21 -0700

    Add some notes, no real changes

--- game_runner.py
+++ game_runner.py
@@ -6,21 +6,23 @@ import sys
 
 arg_replay = "--replay-directory replays/"
 arg_verbosity = "-vvv"
-arg_width = "--width 32"
-arg_height = "--height 32"
+arg_width = "--width 40"
+arg_height = "--height 40"
 bot1_args = "python3 MyBot.py"
-bot2_args = "python3 MyDefaultBot.py"
+bot2_args = "python3 MyTutorialBot.py"
+bot3_args = "python3 MyDefaultBot.py"
+bot4_args = "python3 MyTutorialBot.py"
 #arg_strict = "--strict"
-#arg_seed = "--seed 1234"
+arg_seed = "--seed 1541460138"
 
-args = ["./halite", arg_replay, arg_verbosity, arg_width, arg_height, bot1_args, bot2_args]
+# base jam: 64x64, Map seed is 1541367798
+# base jam: 64x64, Map seed is 1541450737 ???
+# 64x64 4 player 1541450737 # stuck at turn ~82
+# 64x64, 4 player, 1541460138, opponent collision at turn 422
+args = ["./halite", arg_replay, arg_verbosity, arg_width, arg_height, bot1_args, bot2_args, bot3_args, bot4_args]
 
 # run is only available in python 3.5+, prior use subprocess.call
 retval = subprocess.run(args, stdout=subprocess.PIPE, stderr=sys.stderr)
-
-#subprocess.call(["./halite", "--replay-directory replays/ -vvv --width 32 --height 32 \"python3 MyBot.py\" \"python3 MyBot.py\"", "/dev/null"], stdout=subprocess.PIPE)
-
-# CompletedProcess(args=['./halite', '--replay-directory replays/', '-vvv', '--width 32', '--height 32', 'python3 MyBot.py', 'python3 MyBot.py'], returncode=0, stdout=b'')
 
 if retval.returncode != 0:
     print(str(args[0]) + " exited with code " + str(retval.returncode))
