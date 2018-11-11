56b034d2948957b2679e9527f49f24231e73991f
Greg Morgan <gregorytmorgan@gmail.com>
2018-11-10 19:49:16 -0700

    Update fluorine script bin location

--- run_fluorine.sh
+++ run_fluorine.sh
@@ -2,17 +2,16 @@
 
 TargetFile=$(ls -r -t -1  ~/dev/Halite-III-python-bot/replays/*.hlt | tail -1)
 
-
 if [ -f /usr/local/bin/electron ]; then
     ELECTRON=/usr/local/bin/electron
-elif [ -f node_modules/.bin/electron ]; then 
-    ELECTRON=node_modules/.bin/electron
+elif [ -f ./fluorine/node_modules/.bin/electron ]; then 
+    ELECTRON=./fluorine/node_modules/.bin/electron
 else
     echo 'Electron not found'
     exit 1
 fi
 
-$ELECTRON /home/gmorgan/dev/fluorine -o  $TargetFile
+$ELECTRON ./fluorine -o  $TargetFile
 
 
 
