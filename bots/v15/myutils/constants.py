e824311902d3464265e6abc69708245e809955f5
gmorgan <gmorgan@soleranetworks.com>
2018-11-24 16:25:28 -0700

    Version 15 - fix bug in mining rate, speed up spawn of initial ships, add traffic mgmt/enry-exit lanes (this may be a negative)

---
+++ bots/v15/myutils/constants.py
@@ -0,0 +1,40 @@
+#
+#
+#
+
+# This library contains direction metadata to better interface with the game.
+from hlt.positionals import Direction
+
+DEBUG_NONE = 0
+DEBUG_GAME = 1
+DEBUG_SHIP = 2
+DEBUG_NAV = 4
+DEBUG_NAV_METRICS = 8
+DEBUG_GAME_METRICS = 16
+DEBUG_COMMANDS = 32
+DEBUG_SHIP_STATES = 64
+DEBUG_OUTPUT_GAME_METRICS = 128
+
+DEBUG_ALL = DEBUG_GAME | DEBUG_SHIP | DEBUG_NAV | DEBUG_NAV_METRICS | DEBUG_GAME_METRICS | DEBUG_COMMANDS | DEBUG_SHIP_STATES | DEBUG_OUTPUT_GAME_METRICS
+
+DEBUG = DEBUG_NONE # DEBUG_GAME | DEBUG_GAME_METRICS | DEBUG_SHIP_STATES | DEBUG_NAV | DEBUG_NAV_METRICS
+
+# convert a Direction obj back to a string
+DIRECTIONS = {
+    "n": Direction.North,
+    "s": Direction.South,
+    "e": Direction.East,
+    "w": Direction.West,
+    "o": Direction.Still
+}
+
+MIN_LOITER = 4
+MAX_LOITER = 64
+
+MIN_SHIPS = 4
+MAX_SHIPS = 24
+
+STATS_DIR = 'stats'
+
+MINING_RATE_LOOKBACK = 25
+DEPARTURE_DISTANCE = MIN_LOITER - 1
--- game_runner.py
+++ game_runner.py
@@ -8,12 +8,23 @@ arg_replay = "--replay-directory replays/"
 arg_verbosity = "-vvv"
 arg_width = "--width 40"
 arg_height = "--height 40"
+
 bot1_args = "python3 MyBot.py"
-bot2_args = "python3 bots/v14/MyBot.v14.py"
-bot3_args = "python3 bots/MyBot.v10.py"
-bot4_args = "python3 bots/MyBot.v9.py"
+bot2_args = "python3 bots/v15/MyBot.v15.py"
+
+#bot1_args = "python3 candidate.v15.4/MyBot.py"
+#bot2_args = "python3 candidate.v15.3/MyBot.py"
+
+#bot3_args = "python3 bots/MyBot.v10.py"
+#bot4_args = "python3 bots/MyBot.v9.py"
+
 #arg_strict = "--strict"
-arg_seed = "--seed 1542845465"
+arg_seed = "--seed 1543005624" # small sparse, v15 loses
+arg_seed = "--seed 1543007974" # small sparse, v15 loses
+arg_seed = "--seed 1543014634" # dense map with deadlock waiting to dropoff
+
+arg_seed = "--seed 1543094899" # dense map
+
 
 # base jam: 64x64, Map seed is 1541367798
 # base jam: 64x64, Map seed is 1541450737 ???
