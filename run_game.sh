06d562fafe6e3722fa68f299c76d4f78ce8f429a
gmorgan <gmorgan@soleranetworks.com>
2018-11-25 12:59:12 -0700

    Update run_game.sh to accept a bot name arg

--- run_game.sh
+++ run_game.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-./halite --replay-directory replays/ -vvv --width 32 --height 32 "python3 MyBot.py" "python3 MyBot.py"
+if [ -z "$1" ]; then
+   ./halite --replay-directory replays/ -vvv --width 32 --height 32 "python3 MyBot.py" "python3 MyBot.py"
+else
+	./halite --replay-directory replays/ -vvv --width 32 --height 32 "python3 $1" "python3 $1"
+fi
+
