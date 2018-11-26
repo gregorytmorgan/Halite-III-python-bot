9c861b8cd7fadd187b37789460d12b6720625c32
gmorgan <gmorgan@soleranetworks.com>
2018-11-26 09:07:32 -0700

    Add new hotspot-discovery test bot to repo

---
+++ bots/hotspot-discovery/MyBot.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+# Python 3.6
+
+import hlt
+
+from hlt import constants
+
+import logging
+import datetime
+
+# mybot code
+from myutils.utils import *
+from myutils.constants import *
+
+#
+# game setup
+#
+
+""" <<<Game Begin>>> """
+
+# This game object contains the initial game state.
+game = hlt.Game()
+
+ship_states = {}
+
+botName = "MyBot.test-example"
+
+tasked_ships = {}
+
+#
+# game start
+#
+
+game.ready(botName)
+
+if DEBUG & (DEBUG_GAME): logging.info("Game - Successfully created bot! My Player ID is {}. {}".format(game.my_id, "{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now())))
+
+""" <<<Game Loop>>> """
+
+while True:
+    game.update_frame()
+
+    me = game.me
+    game_map = game.game_map
+
+    command_queue = []
+
+    for p_id, player in game.players.items():
+        if p_id != me.id:
+            enemy_base = player.shipyard.position
+            break
+
+    my_ships = me.get_ships()
+
+    # initialize the ship states
+    for ship in my_ships:
+        if not (ship.id in ship_states):
+            if DEBUG & (DEBUG_GAME): logging.info("Game - New ship with ID {}".format(ship.id))
+            me.ship_count += 1
+            ship_states[ship.id] = {
+                "last_seen": game.turn_number,
+                "prior_position": None,
+                "prior_halite_amount": None,
+                "status": "returning",
+                "last_dock": game.turn_number,
+                "christening": game.turn_number,
+                "path": []
+            }
+
+            game.ship_christenings[ship.id] = game.turn_number
+
+            if len(tasked_ships) < 1:
+                logging.info("GAME - Ship {} is tasked".format(ship.id))
+                ship_states[ship.id]["status"] = "tasked"
+                tasked_ships[ship.id] = {
+                    "task_id": 1,
+                    "task_name": "cover_dropoff"
+                }
+
+        # attribs not dependent on save state
+        ship.last_seen = game.turn_number
+
+        # update the current ship based on saved state
+        ship.status = ship_states[ship.id]["status"]
+        ship.path = ship_states[ship.id]["path"]
+        ship.christening = ship_states[ship.id]["christening"]
+        ship.last_dock = ship_states[ship.id]["last_dock"]
+
+    # handle each ship for this turn
+    for ship in my_ships:
+        logging.info("Game - Ship {} at {} has {} halite and is {}".format(ship.id, ship.position, ship.halite_amount, ship.status))
+
+        if ship.id in tasked_ships:
+            if ship.position == enemy_base:
+                logging.info("GAME - Ship {} arrived {}".format(ship.id, enemy_base))
+                move = "o"
+            else:
+                #logging.info("GAME - Ship {} yard: {} enemy_base: {}".format(ship.id, me.shipyard.position, enemy_base))
+                move = game_map.naive_navigate(ship, enemy_base)
+        else:
+            # logic for untasked ships
+            if ship.status == "returning":
+                if ship.position == me.shipyard.position:
+                    ship.status = "exploring"
+                    move = get_move(game, ship, "random", "random")
+                else:
+                    move = game_map.naive_navigate(ship, me.shipyard.position)
+
+            elif ship.halite_amount >= constants.MAX_HALITE / 4:
+                ship.status = "returning"
+                move = game_map.naive_navigate(ship, me.shipyard.position)
+            else:
+                move = get_move(game, ship, "random", "random")
+
+        # move
+        if check_fuel_cost(game, ship) and (game_map[ship.position].halite_amount < constants.MAX_HALITE / 10 or ship.is_full):
+            logging.info("GAME - Ship {} is moving {}".format(ship.id, move))
+            command_queue.append(ship.move(move))
+        else:
+            command_queue.append(ship.stay_still())
+
+        #
+        # save the ship state
+        #
+        ship_states[ship.id]["status"] = ship.status
+        ship_states[ship.id]["path"] = ship.path
+        ship_states[ship.id]["prior_position"] = ship.position
+        ship_states[ship.id]["prior_halite_amount"] = ship.halite_amount
+        ship_states[ship.id]["last_seen"] = ship.last_seen
+        ship_states[ship.id]["christening"] = ship.christening
+        ship_states[ship.id]["last_dock"] = ship.last_dock
+
+    # check for lost ships
+    lost_ship_ids = []
+    for s_id in ship_states:
+        if not me.has_ship(s_id):
+            lost_ship_ids.append(s_id)
+            logging.info("Game - Ship {} lost. Last seen on turn {}".format(s_id, ship_states[s_id]["last_seen"]))
+
+    for s_id in lost_ship_ids:
+        tasked_ships.pop(s_id, None)
+        ship_states.pop(s_id, None)
+
+    # check if we can spawn a ship
+    if ships_are_spawnable(game):
+        command_queue.append(me.shipyard.spawn())
+
+    # Send your moves back to the game environment, ending this turn.
+    game.end_turn(command_queue)
+
+
---
+++ bots/hotspot-discovery/hlt/game_map.py
@@ -0,0 +1,414 @@
+import queue
+
+from . import constants
+from .entity import Entity, Shipyard, Ship, Dropoff
+from .player import Player
+from .positionals import Direction, Position
+from .common import read_input
+import logging
+import time
+
+from myutils.constants import DEBUG, DEBUG_NAV
+
+class MapCell:
+    """A cell on the game map."""
+    def __init__(self, position, halite_amount):
+        self.position = position
+        self.halite_amount = halite_amount
+        self.ship = None
+        self.structure = None
+
+    @property
+    def is_empty(self):
+        """
+        :return: Whether this cell has no ships or structures
+        """
+        return self.ship is None and self.structure is None
+
+    @property
+    def is_occupied(self):
+        """
+        :return: Whether this cell has any ships
+        """
+        return self.ship is not None
+
+    @property
+    def has_structure(self):
+        """
+        :return: Whether this cell has any structures
+        """
+        return self.structure is not None
+
+    @property
+    def structure_type(self):
+        """
+        :return: What is the structure type in this cell
+        """
+        return None if not self.structure else type(self.structure)
+
+    def mark_unsafe(self, ship):
+        """
+        Mark this cell as unsafe (occupied) for navigation.
+
+        Use in conjunction with GameMap.naive_navigate.
+        """
+        self.ship = ship
+
+    def mark_safe(self):
+        """
+        Mark this cell as safe (unoccupied) for navigation.
+
+        """
+        self.ship = None
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __str__(self):
+        return 'MapCell({}, halite={})'.format(self.position, self.halite_amount)
+
+
+class GameMap:
+    """
+    The game map.
+
+    Can be indexed by a position, or by a contained entity.
+    Coordinates start at 0. Coordinates are normalized for you
+    """
+
+    DEBUG = False
+
+    def __init__(self, cells, width, height):
+        self.width = width
+        self.height = height
+        self._cells = cells
+
+    def __getitem__(self, location):
+        """
+        Getter for position object or entity objects within the game map
+        :param location: the position or entity to access in this map
+        :return: the contents housing that cell or entity
+        """
+        if isinstance(location, Position):
+            location = self.normalize(location)
+            return self._cells[location.y][location.x]
+        elif isinstance(location, Entity):
+            return self._cells[location.position.y][location.position.x]
+        return None
+
+    def calculate_distance(self, source, target):
+        """
+        Compute the Manhattan distance between two locations.
+        Accounts for wrap-around.
+        :param source: The source from where to calculate
+        :param target: The target to where calculate
+        :return: The distance between these items
+        """
+        source = self.normalize(source)
+        target = self.normalize(target)
+        resulting_position = abs(source - target)
+        return min(resulting_position.x, self.width - resulting_position.x) + \
+            min(resulting_position.y, self.height - resulting_position.y)
+
+    def normalize(self, position):
+        """
+        Normalized the position within the bounds of the toroidal map.
+        i.e.: Takes a point which may or may not be within width and
+        height bounds, and places it within those bounds considering
+        wraparound.
+        :param position: A position object.
+        :return: A normalized position object fitting within the bounds of the map
+        """
+        return Position(position.x % self.width, position.y % self.height)
+
+    @staticmethod
+    def _get_target_direction(source, target):
+        """
+        Returns where in the cardinality spectrum the target is from source. e.g.: North, East; South, West; etc.
+        NOTE: Ignores toroid
+        :param source: The source position
+        :param target: The target position
+        :return: A tuple containing the target Direction. A tuple item (or both) could be None if within same coords
+        """
+        return (Direction.South if target.y > source.y else Direction.North if target.y < source.y else None,
+                Direction.East if target.x > source.x else Direction.West if target.x < source.x else None)
+
+    def get_unsafe_moves(self, source, destination):
+        """
+        Return the Direction(s) to move closer to the target point, or empty if the points are the same.
+        This move mechanic does not account for collisions. The multiple directions are if both directional movements
+        are viable.
+        :param source: The starting position
+        :param destination: The destination towards which you wish to move your object.
+        :return: A list of valid (closest) Directions towards your target.
+        """
+        source = self.normalize(source)
+        destination = self.normalize(destination)
+        possible_moves = []
+        distance = abs(destination - source)
+        y_cardinality, x_cardinality = self._get_target_direction(source, destination)
+
+        if distance.x != 0:
+            possible_moves.append(x_cardinality if distance.x < (self.width / 2)
+                                  else Direction.invert(x_cardinality))
+        if distance.y != 0:
+            possible_moves.append(y_cardinality if distance.y < (self.height / 2)
+                                  else Direction.invert(y_cardinality))
+        return possible_moves
+
+    def naive_navigate(self, ship, destination):
+        """
+        Returns a singular safe move towards the destination.
+
+        :param ship: The ship to move.
+        :param destination: Ending position
+        :return: A direction.
+        """
+        # No need to normalize destination, since get_unsafe_moves does that
+        for direction in self.get_unsafe_moves(ship.position, destination):
+            target_pos = ship.position.directional_offset(direction)
+            if not self[target_pos].is_occupied:
+                self[target_pos].mark_unsafe(ship)
+                return direction
+
+        return Direction.Still
+
+    def move_cost(self, a, b, move_cost_type = "turns"):
+        # ignore 'a' since the cost is only a function of the current cell
+
+        if move_cost_type == "turns":
+            return 1
+        elif move_cost_type == "halite":
+            return self[a].halite_amount * .1
+        else:
+            raise RuntimeError("Unknown nav move_cost_type: " + str(move_cost_type))
+
+    def heuristic(self, start, current, goal, move_cost_type = "turns"):
+        manhatten = self.calculate_distance(current, goal)
+
+        if move_cost_type == "turns":
+            dx1 = current.x - goal.x
+            dy1 = current.y - goal.y
+            dx2 = start.x - goal.x
+            dy2 = start.y - goal.y
+            cross = abs(dx1 * dy2 - dx2 * dy1)
+            retval = manhatten + cross * 0.001
+        elif move_cost_type == "halite":
+            retval = manhatten * constants.MAX_HALITE
+
+        return retval
+
+    def navigate(self, start, destination, algorithm="astar", args={}):
+        """
+        algorithm: 'astar', 'naive'
+          'astar' - takes args: 'move_cost': 'turns'|'halite'
+          'naive' - takes no args
+          'dock' - takes no args
+        """
+        if algorithm == "astar":
+            move_cost = args["move_cost"] if "move_cost" in args else None
+            path, cost = self.astar(start, destination, move_cost)
+        elif algorithm == "naive":
+            path, cost = self.get_naive_path(start, destination)
+        elif algorithm == "dock":
+            path, cost = self.get_docking_path(start, destination)
+        else:
+            logging.info("Error - Unknown navigate algorithm {}".format(algorithm))
+            path, cost = None, None
+
+        return path, cost
+
+    #
+    # return None if no soln, returns empty list with zero cost if start == end,
+    # otherwise returns a path list and a cumlative cost
+    #
+    def get_naive_path(self, start, destination):
+
+        if start == destination:
+            return [], 0
+
+        umoves = self.get_unsafe_moves(start, destination)
+        first_move = umoves[0]
+
+        first_position =  Position(start.x + first_move[0], start.y + first_move[1])
+        path = [first_position]
+
+        step = 1 if destination.x >= start.x else -1
+        x = first_position.x
+        for i in range(first_position.x + step,  destination.x + step, step):
+            x = i
+            path.append(Position(x, first_position.y))
+
+        step = 1 if destination.y > start.y else -1
+        if destination.y != start.y:
+            for y in range(first_position.y,  destination.y + step, step):
+                path.append(Position(x, y))
+
+        path.reverse()
+
+        return path, len(path)
+
+    def get_docking_path(self, start, dropoff):
+        """
+        dock paths are north and south
+
+        """
+        if start.y == dropoff.y:
+            path = [dropoff, Position(dropoff.x, dropoff.y - 1), Position(start.x, start.y - 1) ] # dock to the North
+            cost = 2
+        else:
+            path, cost = self.get_naive_path(start, Position(dropoff.x, start.y))
+            path.insert(0, dropoff)
+        return path, cost
+
+    #
+    # Get a path using a-star search, cost function can use 'halite' or 'turns'
+    #
+    # return None if no soln, returns empty list with zero cost if start == end,
+    # otherwise returns a path list and a cumlative cost
+    #
+    def astar(self, start, destination, move_cost_type="turns"):
+        astar_start_time = time.time()
+
+        G = {} #Actual movement cost to each position from the start position
+        F = {} #Estimated movement cost of start to end going via this position
+
+        start = self.normalize(start)
+        end = self.normalize(destination)
+
+        if start == end:
+            return [], 0 #Done!
+
+        if self.DEBUG: logging.info("{} -> {}".format(start, end)) # DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG
+
+        #Initialize starting values
+        G[start] = 0
+        F[start] = self.heuristic(start, start, end, move_cost_type)
+
+        closedVertices = set()
+        openVertices = set([start])
+
+        if self.DEBUG: logging.info("start: adding {} to openVertices".format(start)) # DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG
+
+        cameFrom = {}
+
+        while len(openVertices) > 0:
+            #Get the vertex in the open list with the lowest F score
+            current = None
+            currentFscore = None
+
+            for pos in openVertices:
+                if current is None or F[pos] < currentFscore:
+                    if self.DEBUG: logging.info("updating current score with F score ({}) from {}".format(F[pos], pos)) # DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG
+                    currentFscore = F[pos]
+                    current = pos
+
+            #Check if we have reached the goal
+            if current == end:
+                if self.DEBUG: logging.info("reached goal: {}".format(end)) # DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG
+
+                #Retrace our route backward
+                path = [current]
+                while current in cameFrom:
+                    current = cameFrom[current]
+                    path.append(current)
+
+                path.pop() # remove the start point
+
+                if DEBUG & (DEBUG_NAV): logging.info("Nav - Total A* elapsed time {}".format(round(time.time() - astar_start_time, 4))) # DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG
+
+                return path, F[end] # Done!
+
+            #Mark the current vertex as closed
+            openVertices.remove(current)
+            if self.DEBUG: logging.info("removing {} from openVertices".format(current)) # DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG
+
+            closedVertices.add(current)
+            if self.DEBUG: logging.info("adding {} to closedVertices".format(current)) # DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG
+
+            #Update scores for vertices near the current position
+            #for neighbour in graph.get_vertex_neighbours(current):
+            for neighbour in current.get_surrounding_cardinals():
+                neighbour = self.normalize(neighbour)
+                if self.DEBUG: logging.info("neighbour: {}".format(neighbour)) # DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG
+
+                if neighbour in closedVertices:
+                    if self.DEBUG: logging.info("skipping neighbour {}, already checked".format(neighbour)) # DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG
+                    continue #We have already processed this node exhaustively
+
+                candidateG = G[current] + self.move_cost(current, neighbour, move_cost_type)
+
+                if neighbour not in openVertices:
+                    if self.DEBUG: logging.info("Discovered a new cell {}, adding to  openVertices.".format(neighbour)) # DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG
+                    openVertices.add(neighbour) #Discovered a new vertex
+                elif candidateG >= G[neighbour]:
+                    if self.DEBUG: logging.info("Ignoring candiate cell {}, cost is too high at {}".format(neighbour, round(candidateG, 4))) # DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG
+                    continue #This G score is worse than previously found
+
+                #Adopt this G score
+                cameFrom[neighbour] = current
+                G[neighbour] = candidateG
+                H = self.heuristic(start, neighbour, end, move_cost_type)
+                F[neighbour] = G[neighbour] + H
+                if self.DEBUG: logging.info("Neighbour elapsed time {}".format(round(time.time() - astar_start_time, 4)))
+
+        return None, None
+
+    @staticmethod
+    def _generate():
+        """
+        Creates a map object from the input given by the game engine
+        :return: The map object
+        """
+        map_width, map_height = map(int, read_input().split())
+        game_map = [[None for _ in range(map_width)] for _ in range(map_height)]
+        for y_position in range(map_height):
+            cells = read_input().split()
+            for x_position in range(map_width):
+                game_map[y_position][x_position] = MapCell(Position(x_position, y_position),
+                                                           int(cells[x_position]))
+        return GameMap(game_map, map_width, map_height)
+
+    def _update(self):
+        """
+        Updates this map object from the input given by the game engine
+        :return: nothing
+        """
+        # Mark cells as safe for navigation (will re-mark unsafe cells
+        # later)
+        for y in range(self.height):
+            for x in range(self.width):
+                self[Position(x, y)].ship = None
+
+        for _ in range(int(read_input())):
+            cell_x, cell_y, cell_energy = map(int, read_input().split())
+            self[Position(cell_x, cell_y)].halite_amount = cell_energy
+
+    def __repr__(self):
+        map = ""
+        for y in range(self.height):
+            row = ""
+            for x in range(self.width):
+                cell = self[Position(x, y)]
+
+                if cell.is_empty:
+                    s = "{:<6d}".format(cell.halite_amount)
+                elif cell.has_structure:
+                    if cell.is_occupied:
+                        s = "{:<6s}".format("[<" + str(cell.ship.id) + ">]")
+                    else:
+                        s = "{:<6s}".format("[ X ]")
+                else:
+                    s = "{:<6s}".format("<" + str(cell.ship.id) + ">")
+
+                if row == "":
+                    row = row + "{:<3d}{}".format(y, s)
+                else:
+                    row = row + "{}".format(s)
+
+            map = map + row
+
+        return map
