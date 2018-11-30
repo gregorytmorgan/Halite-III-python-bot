96bf21b3acb97f1b864985243c1345036e555f4a
gmorgan <gmorgan@soleranetworks.com>
2018-11-29 17:14:43 -0700

    Add a script to quickly make test candidates

---
+++ make-candidate.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+
+if [ -z "$1" ]; then
+    echo "Candidate number required"
+    exit 1
+fi
+
+n=$1
+
+Candidate="candidate.$n"
+
+if [ -e $Candidate ]; then
+    echo "$Candidate already exists"
+    exit 2
+fi
+
+mkdir $Candidate
+retval=$?
+
+if [ $retval != 0 ]; then
+    echo "mkdir failed with error code $retval"
+    exit 3
+fi
+
+cp MyBot.py $Candidate
+retval=$?
+if [ $retval != 0 ]; then
+    echo "copy MyBot.py failed with error code $retval"
+    exit 4
+fi
+
+cp -r hlt $Candidate
+retval=$?
+if [ $retval != 0 ]; then
+    echo "Copy of hlt directory failed with error code $retval"
+    exit 5
+fi
+
+cp -r myutils $Candidate
+retval=$?
+if [ $retval != 0 ]; then
+    echo "Copy of myutils directory failed with error code $retval"
+    exit 6
+fi
+
+if [ "$2" != "" ]; then
+	echo $2 > $Candidate/readme.txt
+fi
+
+echo "$Candidate done."
+
+
+
+
+
+
+
