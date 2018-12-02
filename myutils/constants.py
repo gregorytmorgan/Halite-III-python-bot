a4b891dd550f60fdbbdd6bb5c83f4e5e392a3dae
gmorgan <gmorgan@soleranetworks.com>
2018-12-01 22:40:18 -0700

    Add comment for each debug constant

--- myutils/constants.py
+++ myutils/constants.py
@@ -2,19 +2,37 @@
 #
 #
 
+# No debug messages
 DEBUG_NONE = 0
+
+# General game flow
 DEBUG_GAME = 1
-DEBUG_SHIP = 2 # not used, repurpose ???
+
+# Misc timings. Complete game, game initialization (cv map), turn, A* path calc
+DEBUG_TIMING = 2
+
+# Detailed logging on path generation
 DEBUG_NAV = 4
+
+# Misc nav related metrics, loiter pt multiples and location, Return trip duration, ...
 DEBUG_NAV_METRICS = 8
+
+# Misc game metrics. Total: Mined, gathered, burned, profit, spent. Turn: Mined gathered,
+# burned, profit, spent, mining rate.  cv Map,
 DEBUG_GAME_METRICS = 16
+
+# Command queue
 DEBUG_COMMANDS = 32
+
+# Ship states
 DEBUG_SHIP_STATES = 64
+
+# Write the game meterics to a file for analysis
 DEBUG_OUTPUT_GAME_METRICS = 128
 
-DEBUG_ALL = DEBUG_GAME | DEBUG_SHIP | DEBUG_NAV | DEBUG_NAV_METRICS | DEBUG_GAME_METRICS | DEBUG_COMMANDS | DEBUG_SHIP_STATES | DEBUG_OUTPUT_GAME_METRICS
+DEBUG_ALL = DEBUG_GAME | DEBUG_TIMING | DEBUG_NAV | DEBUG_NAV_METRICS | DEBUG_GAME_METRICS | DEBUG_COMMANDS | DEBUG_SHIP_STATES | DEBUG_OUTPUT_GAME_METRICS
 
-DEBUG =  DEBUG_NONE # DEBUG_GAME | DEBUG_GAME_METRICS | DEBUG_NAV | DEBUG_OUTPUT_GAME_METRICS | DEBUG_SHIP_STATES
+DEBUG = DEBUG_NONE # DEBUG_GAME | DEBUG_GAME_METRICS | DEBUG_NAV | DEBUG_OUTPUT_GAME_METRICS | DEBUG_SHIP_STATES | DEBUG_TIMING
 
 # convert a Direction obj back to a string
 DIRECTIONS = {
