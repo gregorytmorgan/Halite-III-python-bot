c1de46c7e98267d6ec00e1ecf018ed8b96094b84
gmorgan <gmorgan@soleranetworks.com>
2018-12-07 07:03:22 -0700

    Add v19 bot to the repo, bump the version file

---
+++ bots/v19/hlt/positionals.py
@@ -0,0 +1,158 @@
+from . import commands
+
+from myutils.constants import DIRECTIONS
+
+class Direction:
+    """
+    Holds positional tuples in relation to cardinal directions
+    """
+    North = (0, -1)
+    South = (0, 1)
+    East = (1, 0)
+    West = (-1, 0)
+
+    Still = (0, 0)
+
+    @staticmethod
+    def get_all_cardinals():
+        """
+        Returns all contained items in each cardinal
+        :return: An array of cardinals
+        """
+        return [Direction.North, Direction.South, Direction.East, Direction.West]
+
+    @staticmethod
+    def convert(direction):
+        """
+        Converts from this direction tuple notation to the engine's string notation
+        :param direction: the direction in this notation
+        :return: The character equivalent for the game engine
+        """
+        if direction == Direction.North:
+            return commands.NORTH
+        if direction == Direction.South:
+            return commands.SOUTH
+        if direction == Direction.East:
+            return commands.EAST
+        if direction == Direction.West:
+            return commands.WEST
+        if direction == Direction.Still:
+            return commands.STAY_STILL
+        else:
+            raise IndexError
+
+    @staticmethod
+    def invert(direction):
+        """
+        Returns the opposite cardinal direction given a direction
+        :param direction: The input direction
+        :return: The opposite direction
+        """
+        if direction == Direction.North:
+            return Direction.South
+        if direction == Direction.South:
+            return Direction.North
+        if direction == Direction.East:
+            return Direction.West
+        if direction == Direction.West:
+            return Direction.East
+        if direction == Direction.Still:
+            return Direction.Still
+        else:
+            raise IndexError
+
+    @staticmethod
+    def laterals(direction):
+        """
+        Returns a list of the lateral (left & right) cardinal directions given a direction,
+        Still returns Still
+        :param direction: The input direction
+        :return: The lateral directions
+        """
+        if isinstance(direction,  str):
+            direction = DIRECTIONS[direction]
+
+        if direction == Direction.North or direction == Direction.South:
+            return [Direction.East, Direction.West]
+        elif direction == Direction.East or direction == Direction.West:
+            return [Direction.North, Direction.South]
+        elif direction == Direction.Still:
+            return Direction.Still
+        else:
+            raise IndexError
+
+    @staticmethod
+    def get_adjacent(p):
+        """
+
+        """
+        return [
+            Position(p.x, p.y - 1),        # n   0,-1
+            Position(p.x, + 1, p.y - 1),   # ne  1,-1
+            Position(p.x + 1, p.y),        # e   1, 0
+            Position(p.x + 1, p.y + 1),    # se  1, 1
+            Position(p.x, p.y + 1),        # s      0, 1
+            Position(p.x - 1, p.y + 1),    # sw -1, 1
+            Position(p.x - 1, p.y),        # w  -1, 0
+            Position(p.x - 1, p.y - 1)     # nw -1,-1
+        ]
+
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def directional_offset(self, direction):
+        """
+        Returns the position considering a Direction cardinal tuple
+        :param direction: the direction cardinal tuple
+        :return: a new position moved in that direction
+        """
+        return self + Position(*direction)
+
+    def get_surrounding_cardinals(self):
+        """
+        :return: Returns a list of all positions around this specific position in each cardinal direction
+        """
+        return [self.directional_offset(current_direction) for current_direction in Direction.get_all_cardinals()]
+
+    def __add__(self, other):
+        return Position(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Position(self.x - other.x, self.y - other.y)
+
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+        return self
+
+    def __isub__(self, other):
+        self.x -= other.x
+        self.y -= other.y
+        return self
+
+    def __mul__(self, other):
+        return Position(self.x * other, self.y * other)
+
+    def __imult__(self, other):
+        self.x *= other
+        self.y *= other
+        return self
+
+    def __abs__(self):
+        return Position(abs(self.x), abs(self.y))
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return hash(repr(self))
+
+    def __repr__(self):
+        return "{}({}, {})".format(self.__class__.__name__,
+                                   self.x,
+                                   self.y)
---
+++ bots/v19/myutils/constants.py
@@ -0,0 +1,65 @@
+#
+#
+#
+
+# No debug messages
+DEBUG_NONE = 0
+
+# General game flow
+DEBUG_GAME = 1
+
+# Misc timings. Complete game, game initialization (cv map), turn, A* path calc
+DEBUG_TIMING = 2
+
+# Detailed logging on path generation
+DEBUG_NAV = 4
+
+# Misc nav related metrics, loiter pt multiples and location, Return trip duration, ...
+DEBUG_NAV_METRICS = 8
+
+# Misc game metrics. Total: Mined, gathered, burned, profit, spent. Turn: Mined gathered,
+# burned, profit, spent, mining rate.  cv Map,
+DEBUG_GAME_METRICS = 16
+
+# Command queue
+DEBUG_COMMANDS = 32
+
+# Ship states
+DEBUG_SHIP_STATES = 64
+
+# Write the game meterics to a file for analysis
+DEBUG_OUTPUT_GAME_METRICS = 128
+
+DEBUG_ALL = DEBUG_GAME | DEBUG_TIMING | DEBUG_NAV | DEBUG_NAV_METRICS | DEBUG_GAME_METRICS | DEBUG_COMMANDS | DEBUG_SHIP_STATES | DEBUG_OUTPUT_GAME_METRICS
+
+DEBUG = DEBUG_NONE # DEBUG_GAME | DEBUG_GAME_METRICS | DEBUG_NAV | DEBUG_OUTPUT_GAME_METRICS | DEBUG_SHIP_STATES | DEBUG_TIMING
+
+# convert a Direction obj back to a string
+DIRECTIONS = {
+    "n": (0, -1),
+    "s": (0, 1),
+    "e": (1, 0),
+    "w": (-1, 0),
+    "o": (0, 0)
+}
+
+MIN_LOITER = 4
+MAX_LOITER = 64
+
+MIN_SHIPS = 4
+MAX_SHIPS = 32
+
+STATS_DIR = 'stats'
+
+MINING_RATE_LOOKBACK = 25
+MINING_THRESHOLD_MULT = .1
+
+DEPARTURE_DISTANCE = MIN_LOITER - 1
+
+# Spawn ship up to EXPEDITED_SHIP_COUNT regardless of any other spawn constraints
+EXPEDITED_SHIP_COUNT = 4
+EXPEDITED_DEPARTURE = False
+
+USE_CELL_VALUE_MAP = True
+
+
