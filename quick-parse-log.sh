2ff878d19e5b1f818e92da06022c63e4f869b5ee
gmorgan <gmorgan@soleranetworks.com>
2018-12-20 22:10:22 -0700

    Add a script to check the log for interesting events

---
+++ quick-parse-log.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+
+if [ -z "$1" ]; then
+    file="bot-0.log"
+else
+    file=$1
+fi
+
+if [ ! -e $file ]; then
+    echo "File does not exists $file"
+    exit 1
+fi
+
+retval=$(cat $file | grep -i "warning" | wc -l)
+echo "$retval warnings"
+
+retval=$(cat $file | grep -i "error" | wc -l)
+echo "$retval errors"
+
+retval=$(cat $file | grep "No targets remain" | wc -l)
+echo "$retval No targets remain"
+
+retval=$(cat $file | grep "exceeds available targets" | wc -l)
+echo "$retval exceeds available targets"
+
+retval=$(cat $file | grep "SKIPPED MINING" | wc -l)
+echo "$retval SKIPPED MINING"
+
+#retval=$(cat $file | grep "No retask" | wc -l)
+#echo "$retval No retask"
+
+#retval=$(cat $file | grep "Retask" | wc -l)
+#echo "$retval Retask"
+
+retval=$(cat $file | grep "has a short mining rate" | wc -l)
+echo "$retval mining threshold below cell halite (has a short mining rate)"
+
+retval=$(cat $file | grep "Best block search failed" | wc -l)
+echo "$retval Best block search failed"
+
+retval=$(cat $file | grep "cells have halite < threshold" | wc -l)
+echo "$retval cells have halite < threshold"
+
