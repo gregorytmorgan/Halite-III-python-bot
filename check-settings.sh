042fb708d649dd285664e3b11cd11db0dcc3797f
gmorgan <gmorgan@soleranetworks.com>
2018-12-31 11:32:57 -0700

    Update check-settings to use command-line args instead of a hard coded list

--- check-settings.sh
+++ check-settings.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 #
 
+for botRoot in ${BASH_ARGV[*]}; do
 
-#for databaseName in a b c d e f; do
-# do something like: echo $databaseName
-#done
-
-declare -a arr=("candidate.x" "bots/v23")
-
-for botRoot in "${arr[@]}"
-do
     echo "Bot: $botRoot"
     retval=$(cat $botRoot/myutils/constants.py | grep -i USE_CELL_VALUE_MAP)
     echo "  $retval"
+
     retval=$(cat $botRoot/myutils/constants.py | grep "DEBUG =")
     echo "  $retval"
+
+    retval=$(cat $botRoot/myutils/constants.py | grep "MINING_OVERHEAD =")
+    echo "  $retval"
+
+    retval=$(cat $botRoot/myutils/constants.py | grep "MAX_SHIPS =")
+    echo "  $retval"
+
 done
 
