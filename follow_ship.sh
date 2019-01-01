b997b34de337cdff579b0f4ba839e9447914280c
gmorgan <gmorgan@soleranetworks.com>
2019-01-01 05:48:48 -0700

    Add home sig and usage msg to follow ship script.

--- follow_ship.sh
+++ follow_ship.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 #
 
+function usage () {
+    echo "Usage: $(basename $0) ship_id [log_file]"
+}
+
 if [ -z "$1" ]; then
-    echo "Ship required"
+    echo "Error: ship_id required"
+    usage
     exit 1
+elif [ "$1" == "-h" ]; then
+    usage
+    exit 0
 else
     ship="ship $1 "
 fi
@@ -22,6 +30,6 @@ fi
 initial="new ship\|assigned loiter\|using random loiter"
 milestones="completed assignment\|now returning\|reached loiter\|did not reach loiter\|approached loiter"
 sos="assigned to respond\|diverted from assignment"
-terminal="completed dropoff\|lost"
+terminal="completed dropoff\|lost\|now homing"
 
 cat $file | grep -i "$ship" | grep -i "$initial\|$sos\|$milestones\|$terminal"
