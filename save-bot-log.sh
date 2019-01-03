dfe27412a4a998b5d71a35e04c7fd2b6d87d5767
gmorgan <gmorgan@soleranetworks.com>
2019-01-02 20:22:00 -0700

    Update the save-log script to save all logs not just bot-0. Also use mv instead of cp

--- save-bot-log.sh
+++ save-bot-log.sh
@@ -3,20 +3,31 @@
 tstamp=$(date +"%s")
 botlog="bot-0.log"
 
+
+files=$(ls -rt1 bot-?.log)
+
+
+
 if [ -z $1 ]; then
-	seed=$tstamp
+	tag=$tstamp
 else
-	seed=$1
+	tag=$1
 fi
+
+# override the tag for now
+#tag=$tstamp
 
 if [ ! -d "logs" ]; then
 	/bin/mkdir -p logs
 fi
 
-/bin/cp $botlog logs/bot-log-0.$seed.log
-retval=$?
+for f in $files; do
 
-if [ "$retval" != "0" ]; then
-	/bin/echo "Log copy failed with error code $retval"
-fi
+    /bin/mv $f logs/$f.$tag
+    retval=$?
 
+    if [ "$retval" != "0" ]; then
+	/bin/echo "$f move failed with error code $retval"
+    fi
+    
+done
