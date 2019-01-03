608ddd8ad77853d4e8c7b1acae165e0f4f00df5b
gmorgan <gmorgan@soleranetworks.com>
2019-01-03 12:26:57 -0700

    Add a usage msg to the check-settings script

--- check-settings.sh
+++ check-settings.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #
 
+if [ -z "$1" ]; then
+    echo "Usage: $0 bot_directory [bot_directory ...]"
+fi
+
 for botRoot in ${BASH_ARGV[*]}; do
 
     echo "Bot: $botRoot"
-    retval=$(cat $botRoot/myutils/constants.py | grep -i USE_CELL_VALUE_MAP)
+    retval=$(cat $botRoot/myutils/constants.py | grep -i "USE_CELL_VALUE_MAP")
     echo "  $retval"
 
     retval=$(cat $botRoot/myutils/constants.py | grep "DEBUG =")
@@ -14,6 +18,9 @@ for botRoot in ${BASH_ARGV[*]}; do
     #echo "  $retval"
 
     retval=$(cat $botRoot/myutils/constants.py | grep "MAX_SHIPS =")
+    echo "  $retval"
+    
+    retval=$(cat $botRoot/myutils/constants.py | grep "CV_MINING_RATE_MULTIPLIER =")
     echo "  $retval"
 
 done
