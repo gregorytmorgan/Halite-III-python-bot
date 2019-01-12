9af59da8c0cada67348b2851ab55abffe9cb486e
gmorgan <gmorgan@soleranetworks.com>
2019-01-12 12:40:00 -0700

    Update game_runner arg handling so it's easier to change args

--- game_runner.py
+++ game_runner.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-# Python 3.6
+#!/usr/bin/env python3.6
 
 import subprocess
 import sys
@@ -7,40 +6,54 @@ import shutil
 import time
 import random
 
+print(sys.version)
+
 arg_replay = "--replay-directory replays/"
 arg_verbosity = "-vvv"
-arg_width = "--width 32"
-arg_height = "--height 32"
+arg_width = "--width 48"
+arg_height = "--height 48"
 
 # pypy example, other than invoking with pypy3 no other changes are needed, though
 # in the case of the halite.io server, an extension of .pypy is required
 #bot1_args = "./pypy3 -E MyBot.py"
 
-bot1_args = "python3 MyBot.py"
+# Note: Nebeans has trouble running bots with diff versions of python.  Make sure the
+# Netbeans python version matches the cmd line version
+bot1_args = "python3.6 MyBot.py"
 
 opponents = [
-    "python3 bots/v26/MyBot.py",
-    "python3 bots/v25/MyBot.py",
-    "python3 bots/v27/MyBot.py"
+    "python3.6 bots/v26/MyBot.py",
+    "python3.6 bots/v25/MyBot.py",
+    "python3.6 bots/v27/MyBot.py"
 ]
 
 # True = 4p with oppenent shuffle
 if False:
     oppenent_count = len(opponents)
-    idx = random.randint(1, oppenent_count)
-    bot2_args = opponents[idx  % oppenent_count]
+    idx = random.randint(0, oppenent_count) % oppenent_count
+    bot2_args = opponents[idx]
     bot3_args = opponents[(idx + 1)  % oppenent_count]
     bot4_args = opponents[(idx + 2)  % oppenent_count]
+    more_opponents = [bot3_args, bot4_args]
 else:
     bot2_args = opponents[2]
+    more_opponents = []
 
 #arg_strict = "--strict"
-arg_seed = "--seed 1543014634" # dense map, 301k halite
-#arg_seed = "--seed 1543094899" # dense map, 265k halite, dense around base
+
+seed = None
+#seed = 543014634 # dense map, 301k halite
+#seed = 1543094899 # dense map, 265k halite, dense around base
+#seed = 1547265756
+#seed = 1547280599 # good for dropoffs @64 , not so much @ 32
+
+seed = 1547320475
+
+arg_seed = ["--seed " + str(seed)] if seed else []
 
 # , bot3_args, bot4_args
 # , arg_width, arg_height
-args = ["./halite", arg_seed, arg_replay, arg_verbosity, bot1_args, bot2_args, arg_width, arg_height]
+args = ["./halite", arg_replay, arg_verbosity, arg_width, arg_height, bot1_args, bot2_args] + more_opponents + arg_seed
 
 # run is only available in python 3.5+, prior use subprocess.call
 retval = subprocess.run(args, stdout=subprocess.PIPE, stderr=sys.stderr)
