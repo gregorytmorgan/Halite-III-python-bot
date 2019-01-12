cafc86b1cc4f54ef1a982317a0cd62d5c9935fd1
gmorgan <gmorgan@soleranetworks.com>
2019-01-12 13:01:26 -0700

    Update run_game script to used python3.6

--- run_game.sh
+++ run_game.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ -z "$1" ]; then
-   ./halite --replay-directory replays/ -vvv --width 32 --height 32 "python3 MyBot.py" "python3 MyBot.py"
+   ./halite --replay-directory replays/ -vvv --width 32 --height 32 "python3.6 MyBot.py" "python3.6 MyBot.py"
 else
-	./halite --replay-directory replays/ -vvv --width 32 --height 32 "python3 $1" "python3 $1"
+	./halite --replay-directory replays/ -vvv --width 32 --height 32 "python3.6 $1" "python3.6 $1"
 fi
 
